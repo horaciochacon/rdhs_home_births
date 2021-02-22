67581b6549d8c34b97f8a5605e02b7d043928dd1
horaciochacon <horacio.chacon.t@upch.pe>
2021-02-21 23:48:21 -0500

    Add estimates_forest_plot output

--- src/3_get_estimates.R
+++ src/3_get_estimates.R
@@ -1,5 +1,6 @@
 library(srvyr)
 library(sjlabelled)
+library(tidyverse)
 
 final <- read_rds("data/final.rds")
 
@@ -14,6 +15,26 @@ estimates_countries <- design %>%
   summarise(prop_dom = survey_mean(parto_domiciliario,vartype = "ci"))
 
 write_excel_csv(estimates_countries,"output/estimates_countries.csv")
+
+# Calculate estimates for forest plot STATA input
+estimates_forest_plot <- design %>% 
+  group_by(RegionWorldBank, SubregionName, CountryName, SurveyYear) %>% 
+  summarise(
+    num_we = survey_total(parto_domiciliario),
+    denom_we = survey_total(parto_institucional),
+    num_unwe = unweighted(sum(parto_domiciliario, na.rm = TRUE)),
+    denom_unwe = unweighted(sum(parto_institucional, na.rm = TRUE)),
+    prop_dom = survey_mean(parto_domiciliario,vartype = c("ci","se"))
+    ) %>% 
+  mutate(
+    denom_we = num_we + denom_we,
+    denom_unwe = num_unwe + denom_unwe
+    ) %>% 
+  select(tgroup = RegionWorldBank, SubregionName, study = CountryName,
+         author = CountryName, year = SurveyYear, num_we, denom_we, num_unwe,
+         denom_unwe, prop_dom, prop_dom_se, prop_dom_low, prop_dom_upp)
+
+write_excel_csv(est,"output/estimates_forest_plot.csv")
 
 # Calculate estimates of home delivery prevalence per country per Wealth Quintile
 estimates_wealth_quintile <- design %>% 
